f9b49f4b8848de74bdfa42a95e023bdbb9d8b2e1
Zephyr709 <jdcooper.dev@gmail.com>
2023-03-13 20:41:38 -0230

    JS organization

--- tests.js
+++ tests.js
@@ -1,8 +1,11 @@
+//Contstants
+
 const generateButton = document.getElementById('generateButton');
 
+//Variables
 let numQs, maxNum, minNum = 0;
 
-
+//Functions
 function randInt(min, max) {
     // A proper random integer function; min/max inclusive. credit: W3Schools.com
     return Math.floor(Math.random() * (max - min + 1) + min);
@@ -17,3 +20,5 @@ function getTestParameters() {
 
 
 }
+
+//Event handlers
