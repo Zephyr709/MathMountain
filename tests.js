9f84811b095cd023b2aaaddf91f10c65eb2bd518
Zephyr709 <jdcooper.dev@gmail.com>
2023-03-13 21:36:28 -0230

    pseudo code for operating test-window

--- tests.js
+++ tests.js
@@ -24,4 +24,12 @@ function getTestParameters() {
 generateButton.addEventListener('click', (event) => {
     event.preventDefault();
     getTestParameters();
+    //1.Math: generate a question
+    //2.QNA injection: replace inner html of QNA with question 
+    //3.Log injection: on input event: send question, input answer, correct answer to an html element
+    //  then append the generated element to log section
+    //4.qlog injection: compare input answer vs correct answer send to an html element 
+    //  then append to qlog
+    //5.Math:genereate a question 
+    //6. QNA Injection replace inner html of QNA with new question
 });
